9b5b7aaa9fa0dd88439d923dd45fe6d2195516e2
zkrem56 <88634420+zkrem56@users.noreply.github.com>
2021-10-18 21:24:26 -0500

    Updated Forms and User abilites
    Data is able to be collected from all text boxes in the web application and added User name to all pages so that their username will be present on the webpage if they are logged in.

--- src/main/java/com/example/stationfinder/LibService.java
+++ src/main/java/com/example/stationfinder/LibService.java
@@ -15,40 +15,210 @@ public class LibService {
 	@Autowired
 	private JdbcTemplate template;
 
-	public int max_count = 380;
+	public int min = 0, max = 0;
 	public LibService(JdbcTemplate template) {
 		System.out.println("Service Layer is created");
 	}
 	
 	//Return all the libraries
-	public List<Library> getAllTheLibraries(){
+	public List<Library> getAllTheLibraries(Library library, int s){
 		
 		List<Library> list = new ArrayList<>();
 		
-		for(int i = 1; true; i++) {
-			try {
-				list.add(new Library(template.queryForObject("select ID from Libraries where ID = " + i, Integer.class),
-						template.queryForObject("select Library_Name from Libraries where ID = " + i, String.class),
-						template.queryForObject("select Branch_Name from Libraries where ID = " + 1, String.class),
-						template.queryForObject("select Mailing_Address from Libraries where ID = " + i, String.class),
-						template.queryForObject("select City from Libraries where ID = " + i, String.class),
-						template.queryForObject("select Library_Email_Address from Libraries where ID = " + i, String.class),
-						template.queryForObject("select ZIP_Code from Libraries where ID = " + i, Integer.class),
-						template.queryForObject("select County from Libraries where ID = " + i, String.class)));
-			}
-			catch (Exception e) {
-				System.out.println("Did not work");
-				System.out.println(e.toString());
+		switch(s){
+			case 0:
+				try {
+					min = template.queryForObject("select min(ID) from Libraries where Library_Name = '"+library.getLibName()+"'", Integer.class);
+					max = template.queryForObject("select mix(ID) from Libraries where Library_Name = '"+library.getLibName()+"'", Integer.class);
+				}catch(Exception e) {
+					list.add(new Library("Search Not Found"));
+					return list;
+				}
+				
+				
+				if(min == max) {
+					try {
+						list.add(new Library(
+								template.queryForObject("select ID from Libraries where Library_Name = '"+library.getLibName()+"'", Integer.class),
+								template.queryForObject("select Library_Name from Libraries where Library_Name = '"+library.getLibName()+"'", String.class),
+								template.queryForObject("select Branch_Name from Libraries where Library_Name = '"+library.getLibName()+"'", String.class),
+								template.queryForObject("select Mailing_Address from Libraries where Library_Name = '"+library.getLibName()+"'", String.class),
+								template.queryForObject("select City from Libraries where Library_Name = '"+library.getLibName()+"'", String.class),
+								template.queryForObject("select Library_Email_Address from Libraries where Library_Name = '"+library.getLibName()+"'", String.class),
+								template.queryForObject("select ZIP_Code from Libraries where Library_Name = '"+library.getLibName()+"'", Integer.class),
+								template.queryForObject("select County from Libraries where Library_Name = '"+library.getLibName()+"'", String.class)));
+					}catch(Exception e) {
+						
+					}
+				}
+				else {
+					for(int i = min; i <= max; i++) {
+						try {
+							
+							if(library.getCounty().equalsIgnoreCase(template.queryForObject("select Library_Name from Libraries where ID = "+i, String.class))) {
+								list.add(new Library(
+										template.queryForObject("select ID from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select Library_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Branch_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Mailing_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select City from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Library_Email_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select ZIP_Code from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select County from Libraries where ID = "+i, String.class)));
+							}
+						}catch(Exception e) {
+							
+						}
+					}
+				}
+				
 				break;
-			}
-		
+			case 1:
+				try {
+					min = template.queryForObject("select min(ID) from Libraries where City = '"+library.getCity()+"'", Integer.class);
+					max = template.queryForObject("select max(ID) from Libraries where City = '"+library.getCity()+"'", Integer.class);
+				}catch(Exception e) {
+					list.add(new Library("Search Not Found"));
+					return list;
+				}
+				if(min == max) {
+					try {
+						list.add(new Library(
+								template.queryForObject("select ID from Libraries where City = '"+library.getCity()+"'", Integer.class),
+								template.queryForObject("select Library_Name from Libraries where City = '"+library.getCity()+"'", String.class),
+								template.queryForObject("select Branch_Name from Libraries where City = '"+library.getCity()+"'", String.class),
+								template.queryForObject("select Mailing_Address from Libraries where City = '"+library.getCity()+"'", String.class),
+								template.queryForObject("select City from Libraries where City = '"+library.getCity()+"'", String.class),
+								template.queryForObject("select Library_Email_Address from Libraries where City = '"+library.getCity()+"'", String.class),
+								template.queryForObject("select ZIP_Code from Libraries where City = '"+library.getCity()+"'", Integer.class),
+								template.queryForObject("select County from Libraries where City = '"+library.getCity()+"'", String.class)));
+					}catch(Exception e) {
+						
+					}
+				}
+				else {
+					for(int i = min; i <= max; i++) {
+						try {
+							
+							if(library.getCity().equalsIgnoreCase(template.queryForObject("select City from Libraries where ID = "+i, String.class))) {
+								list.add(new Library(
+										template.queryForObject("select ID from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select Library_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Branch_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Mailing_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select City from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Library_Email_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select ZIP_Code from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select County from Libraries where ID = "+i, String.class)));
+							}
+						}catch(Exception e) {
+							
+						}
+					}
+				}
+				break;
+			case 2:
+				try {
+					min = template.queryForObject("select min(ID) from Libraries where County = '"+library.getCounty()+"'", Integer.class);
+					max = template.queryForObject("select max(ID) from Libraries where County = '"+library.getCounty()+"'", Integer.class);
+				}catch(Exception e) {
+					list.add(new Library("Search Not Found"));
+					return list;
+				}
+				
+				if(max == min) {
+					try {
+						list.add(new Library(
+								template.queryForObject("select ID from Libraries where County = '"+library.getCounty()+"'", Integer.class),
+								template.queryForObject("select Library_Name from Libraries where County = '"+library.getCounty()+"'", String.class),
+								template.queryForObject("select Branch_Name from Libraries where County = '"+library.getCounty()+"'", String.class),
+								template.queryForObject("select Mailing_Address from Libraries where County = '"+library.getCounty()+"'", String.class),
+								template.queryForObject("select City from Libraries where County = '"+library.getCounty()+"'", String.class),
+								template.queryForObject("select Library_Email_Address from Libraries where County = '"+library.getCounty()+"'", String.class),
+								template.queryForObject("select ZIP_Code from Libraries where County = '"+library.getCounty()+"'", Integer.class),
+								template.queryForObject("select County from Libraries where County = '"+library.getCounty()+"'", String.class)));
+					}catch(Exception e) {
+						
+					}
+				}
+				else {
+					for(int i = min; i <= max; i++) {
+						try {
+							
+							if(library.getCounty().equalsIgnoreCase(template.queryForObject("select County from Libraries where ID = "+i, String.class))) {
+								list.add(new Library(
+										template.queryForObject("select ID from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select Library_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Branch_Name from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Mailing_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select City from Libraries where ID = "+i, String.class),
+										template.queryForObject("select Library_Email_Address from Libraries where ID = "+i, String.class),
+										template.queryForObject("select ZIP_Code from Libraries where ID = "+i, Integer.class),
+										template.queryForObject("select County from Libraries where ID = "+i, String.class)));
+							}
+						}catch(Exception e) {
+							
+						}
+					}
+				}
+				break;
+			case 3:
+				try {
+					list.add(new Library(
+							template.queryForObject("select ID from Libraries where Mailing_Address = '"+library.getAddr()+"'", Integer.class),
+							template.queryForObject("select Library_Name from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class),
+							template.queryForObject("select Branch_Name from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class),
+							template.queryForObject("select Mailing_Address from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class),
+							template.queryForObject("select City from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class),
+							template.queryForObject("select Library_Email_Address from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class),
+							template.queryForObject("select ZIP_Code from Libraries where Mailing_Address = '"+library.getAddr()+"'", Integer.class),
+							template.queryForObject("select County from Libraries where Mailing_Address = '"+library.getAddr()+"'", String.class)));
+				}catch(Exception e) {
+					
+				}
+				break;
+			case 4:
+				try {
+					list.add(new Library(
+							template.queryForObject("select ID from Libraries where ZIP_Code = "+library.getZip(), Integer.class),
+							template.queryForObject("select Library_Name from Libraries where ZIP_Code = "+library.getZip(), String.class),
+							template.queryForObject("select Branch_Name from Libraries where ZIP_Code = "+library.getZip(), String.class),
+							template.queryForObject("select Mailing_Address from Libraries where ZIP_Code = "+library.getZip(), String.class),
+							template.queryForObject("select City from Libraries where ZIP_Code = "+library.getZip(), String.class),
+							template.queryForObject("select Library_Email_Address from Libraries where ZIP_Code = "+library.getZip(), String.class),
+							template.queryForObject("select ZIP_Code from Libraries where ZIP_Code = "+library.getZip(), Integer.class),
+							template.queryForObject("select County from Libraries where ZIP_Code = "+library.getZip(), String.class)));
+				}catch(Exception e) {
+					
+				}
+				break;
+			case 5:
+				try {
+					list.add(new Library(
+							template.queryForObject("select ID from Libraries where Branch_Name = '"+library.getBranchName()+"'", Integer.class),
+							template.queryForObject("select Library_Name from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class),
+							template.queryForObject("select Branch_Name from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class),
+							template.queryForObject("select Mailing_Address from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class),
+							template.queryForObject("select City from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class),
+							template.queryForObject("select Library_Email_Address from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class),
+							template.queryForObject("select ZIP_Code from Libraries where Branch_Name = '"+library.getBranchName()+"'", Integer.class),
+							template.queryForObject("select County from Libraries where Branch_Name = '"+library.getBranchName()+"'", String.class)));
+				}catch(Exception e) {
+					
+				}
+				break;
+			default:
+				list.add(new Library("Search Not Found"));
+				break;
 		}
+		
+		
 		return list;
 	}
 	
 	//Save the Player
 	public void saveLibrary(Library library) {
-		template.update("insert into Libraries(ID, Library_Name, Branch_Name, Mailing_Address, City, State, ZIP_Code, County, Library_Email_Address) values(?, ?, ?, ?, ?, ?, ?, ?, ?)", max_count, library.getLibName(), library.getBranchName(), library.getAddr(), library.getCity(), null, library.getZip(), library.getCounty(), library.getEmail());
+		template.update("insert into Libraries(ID, Library_Name, Branch_Name, Mailing_Address, City, State, ZIP_Code, County, Library_Email_Address) values(?, ?, ?, ?, ?, ?, ?, ?, ?)", template.queryForObject("select max(ID) from Libraries", Integer.class), library.getLibName(), library.getBranchName(), library.getAddr(), library.getCity(), null, library.getZip(), library.getCounty(), library.getEmail());
 	}
 	
 	//Update the Library
@@ -82,7 +252,6 @@ public class LibService {
 				} catch(Exception e) {
 					System.out.println(e.toString());
 				}
-				
 	}
 	
 	//Remove Library
